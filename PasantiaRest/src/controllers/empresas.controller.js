3b4888b2aa92cac4fcbb7b9bdd61757bdf3a6da9
pablogb83 <gaionepablo@gmail.com>
2021-10-26 15:04:16 -0300

    limpiando

--- PasantiaRest/src/controllers/empresas.controller.js
+++
@@ -1,9 +0,0 @@
-const Empresa = require("../models/Empresa");
-
-const getAll = async (request, response) => {
-    return response.status(200).json(Empresa.getAll());
-}
-
-module.exports = {
-    getAll
-}
--- PasantiaRest/src/routes/Empresa.js
+++
@@ -1,8 +0,0 @@
-const Router = require("express");
-const empresasController = require("../controllers/empresas.controller");
-
-const router = Router();
-
-router.get('/', empresasController.getAll);
-
-module.exports = router;
