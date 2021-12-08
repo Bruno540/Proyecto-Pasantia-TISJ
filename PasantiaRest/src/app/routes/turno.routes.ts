adfbb4e259f3cd448541277d5f21418513d41567
Agus458 <80288121+Agus458@users.noreply.github.com>
2021-12-08 16:16:11 -0300

    correccion permisos de rutas

--- PasantiaRest/src/app/routes/turno.routes.ts
+++ PasantiaRest/src/app/routes/turno.routes.ts
@@ -1,25 +1,27 @@
 import { Router } from "express";
 import * as TurnoController from "../controllers/turno.controller"
 import { handleRequest } from "../middlewares/error.middleware";
+import { isLoggedIn } from "../middlewares/isLoggedIn";
+import { tieneRol } from "../middlewares/tieneRol";
 
 const router = Router();
 
 router.get('/tipos', handleRequest(TurnoController.getTipos));
 
-router.get('/', handleRequest(TurnoController.get));
+router.get('/', [isLoggedIn, tieneRol(["Administrador", "Empresa"])], handleRequest(TurnoController.get));
 
 router.get('/filtered', handleRequest(TurnoController.getTurnosFiltered));
 
-router.get('/:id', handleRequest(TurnoController.getById));
+router.get('/:id', [isLoggedIn, tieneRol(["Administrador", "Empresa"])], handleRequest(TurnoController.getById));
 
 router.get('/tools/proximos', handleRequest(TurnoController.getProximos));
 
 router.get('/tools/live', handleRequest(TurnoController.getTurnosLive));
 
-router.post('/', handleRequest(TurnoController.post));
+router.post('/', [isLoggedIn, tieneRol(["Administrador", "Empresa"])], handleRequest(TurnoController.post));
 
-router.put('/:id', handleRequest(TurnoController.put));
+router.put('/:id', [isLoggedIn, tieneRol(["Administrador", "Empresa"])], handleRequest(TurnoController.put));
 
-router.delete('/:id', handleRequest(TurnoController._delete));
+router.delete('/:id', [isLoggedIn, tieneRol(["Administrador", "Empresa"])], handleRequest(TurnoController._delete));
 
 export default router;
